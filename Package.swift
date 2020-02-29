10476bafba4f434ee8c214b5c9a728f8fe1f1b0a
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-28 23:28:04 -0600

    Do not require Catalina.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "sqlite-nio",
     platforms: [
-       .macOS(.v10_15),
+       .macOS(.v10_14),
        .iOS(.v13)
     ],
     products: [
